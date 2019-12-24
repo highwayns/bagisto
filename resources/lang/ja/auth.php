4e3ed2d964695ec52542c7771da8b77a4c3a376e
vagrant <vagrant@homestead>
2019-12-24 08:58:58 +0000

    add ja

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '資格情報が正しくない.',
+    'throttle' => 'ログイン失敗回数が多いです. :seconds 秒以降再度試してください.',
+
+];
---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'パスワードが少なくとも6文字です.',
+    'reset' => 'パスワードを再設定しました!',
+    'sent' => 'パスワード再設定リンクをEメールで送信しました!',
+    'token' => 'パスワード再設定トークンが正しくない.',
+    'user' => "Eメールアドレスが正しくない.",
+
+];
